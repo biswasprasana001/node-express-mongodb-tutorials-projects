872a8f0602fbccfae51e6a1b786ddbed27c7897b
biswasprasana001 <biswasprasana004@gmail.com>
2022-03-11 00:39:48 +0530

    One project Done

---
+++ 08-file-upload/starter/controllers/uploadsController.js
@@ -0,0 +1,46 @@
+const Product = require("../models/Product");
+const path = require("path");
+const { StatusCodes } = require("http-status-codes");
+const CustomError = require("../errors");
+const cloudinary = require("cloudinary").v2;
+
+const uploadProductImageLocal = async (req, res) => {
+  console.log(req.files);
+  if (!req.files) {
+    throw new CustomError.BadRequestError("No File Uploaded");
+  }
+  const productImage = req.files.image;
+  if (!productImage.mimetyep.startsWith("image")) {
+    throw new CustomError.BadRequestError("Please Upload Image");
+  }
+  const maxSize = 1024 * 1024;
+  if (productImage.size > maxSize) {
+    throw new CustomError.BadRequestError("Please upload image smaller 1KB");
+  }
+  // console.log(productImage);
+  const imagePath = path.join(
+    __dirname,
+    "../public/uploads/" + `${productImage.name}`
+  );
+  await productImage.mv(imagePath);
+  return res
+    .status(StatusCodes.OK)
+    .json({ image: { src: `/uploads/${productImage.name}` } });
+};
+
+const uploadProductImage = async (req, res) => {
+  // console.log(req.files.image);
+  const result = await cloudinary.uploader.upload(
+    req.files.image.tempFilePath,
+    {
+      use_filename: true,
+      folder: "file-upload",
+    }
+  );
+  fs.unlinkSync(req.files.image.tempFilePath);
+  return res.status(StatusCodes.OK).json({ image: { src: result.secure_url } });
+};
+
+module.exports = {
+  uploadProductImage,
+};
