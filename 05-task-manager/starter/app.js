4156e6767b72a230d6cd8e5f01b928ad6d455e47
biswasprasana001 <biswasprasana004@gmail.com>
2022-05-02 14:36:58 +0530

    I have to see where the error is

--- 05-task-manager/starter/app.js
+++ 05-task-manager/starter/app.js
@@ -1,1 +1,26 @@
-console.log('Task Manager App')
+const express = require("express");
+const app = express();
+const tasks = require("./routes/tasks");
+const connectDB = require("./db/connect");
+require("dotenv").config();
+const errorHandlerMiddleware = require("./middleware/error-handler");
+
+// middleware
+app.use(express.json());
+
+// routes
+app.use("/api/v1/tasks", tasks);
+app.use(errorHandlerMiddleware);
+
+const port = 3000;
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, console.log(`server is listening to port ${port}`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
