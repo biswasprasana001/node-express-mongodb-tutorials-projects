2c3fcf243c0c4bf2915733ad7e5fbddcceedee5d
biswasprasana001 <biswasprasana004@gmail.com>
2022-05-04 16:12:54 +0530

    Everything set as it was before

--- 05-task-manager/starter/middleware/error-handler.js
+++
@@ -1,9 +0,0 @@
-const { CustomAPIError } = require("../error/custom-error");
-const errorHandlerMiddleware = (err, req, res, next) => {
-  if (err instanceof CustomAPIError) {
-    return res.status(err.status).json({ msg: err.message });
-  }
-  return res.status(500).json({ msg: err.message });
-};
-
-module.exports = errorHandlerMiddleware;
